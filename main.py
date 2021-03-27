5622dfd6e4a282ae942c80baa95f008a0a0a6fbb
AlvinDing11 <alvin.ding@ocsbstudent.ca>
2021-03-27 15:56:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+diam = input("Enter your diameter here: ")
+piee = (3.141)
+circum = float(diam) * float(piee)
+
+radi = float(diam) / 2
+area = float(piee) * float(radi) ** 2
+
+circum = str(circum)
+area = str(area)
+
+
+print("Circumference = " + circum)
+print("Area = " + area)
